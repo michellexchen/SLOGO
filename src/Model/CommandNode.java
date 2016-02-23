06b66339c2af400cdd812ae866e0e93adb26f0c9
Mario Oliver <mar.mariooliver93@gmail.com>
2016-02-23 14:36:17 -0500

    Tried to go through generic type usage to use our possible commands data struct

--- src/Model/CommandNode.java
+++ src/Model/CommandNode.java
@@ -1,18 +1,28 @@
 package Model;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public abstract class CommandNode {
 
-	private static final Map<String, String> commandInfo = new TreeMap<String, String>();
+	private CommandsDataStruct commandsClass;
+	private List<?> possibleCommands;
+	
+	public CommandNode(){
+		commandsClass = new CommandsDataStruct();
+		possibleCommands = commandsClass.getInstance().grabPossibleCommands();
+	}
 	
 	public abstract void evaluate(String myCommand);
 	
 	public abstract void intermediateCanvasUpdate();
 	
 	public void determineTypeNArgs(){
-		
+	}
+	
+	private List<?> getCommands(){
+		return possibleCommands;
 	}
 	
 	
