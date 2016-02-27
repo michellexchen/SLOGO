aafb2eae75fce41cc041a8dcf2d5527e3ed7ebf5
Hunter <narendly@gmail.com>
2016-02-27 11:37:39 -0500

    MainModel created

--- src/Controller/MainDriver.java
+++ src/Controller/MainDriver.java
@@ -3,20 +3,19 @@ package Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import Model.CommandTree;
+import Model.MainModel;
 import Model.Turtle;
 import View.MainView;
 import View.SLogoException;
 
 public class MainDriver extends Application {
 
-	private final int VIEW_WIDTH = 1000;
-	private final int VIEW_HEIGHT = 750;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	public void start(Stage myStage) throws Exception {
+	public void start(Stage myStage) throws SLogoException {
 		MainView myView = new MainView();
 		try {
 			myView.addProject();
@@ -24,15 +23,9 @@ public class MainDriver extends Application {
 		} catch (SLogoException e) {
 			myView.showError(e);
 		}
-		createBackend();
+		MainModel myModel = new MainModel();
+		myModel.createBackend();
 	}
 
-	private void createBackend() {
-		Turtle ogTurt = new Turtle("OG", VIEW_WIDTH / 2, VIEW_HEIGHT / 2, true, 0, false);
-		//myRoot.getChildren().add(ogTurt.getTurtle());
-		TreeFactory tf = new TreeFactory();
-		String command = "forward 50";
-		CommandTree myTree = tf.makeTree(command, ogTurt);
-		myTree.traverse(ogTurt.getState());
-	}
+
 }
