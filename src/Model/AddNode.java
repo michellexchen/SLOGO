176dded71dab25b78f441e294b7a4ad87b4e49f9
Mario Oliver <mar.mariooliver93@gmail.com>
2016-03-01 13:34:06 -0500

    refactoring

--- src/Model/AddNode.java
+++ src/Model/AddNode.java
@@ -2,24 +2,21 @@ package Model;
 
 import Controller.SLogoException;
 
-public class AddNode extends CommandNode{
+public class AddNode extends CommandNode {
 
-		
-		private static final int NUM_CHILDREN = 1;
+	private static final int NUM_CHILDREN = 1;
 
-		public AddNode(){
-			setNumChildren(NUM_CHILDREN);
-		}
-
-		
+	public AddNode() {
+		setNumChildren(NUM_CHILDREN);
+	}
 
 	@Override
 	public double evaluate(CharacterState state) throws SLogoException {
 		double result = 0;
-		for(int x=0; x<getChildren().size(); x++){
+		for (int x = 0; x < getChildren().size(); x++) {
 			result += getChildren().get(x).evaluate(state);
 		}
-		return -1*result;
+		return -1 * result;
 	}
 
 }
