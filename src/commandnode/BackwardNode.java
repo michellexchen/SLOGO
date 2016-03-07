77b59689f3b8ce92a92b7903bde75579cc424611
AaronNewman <agn9@duke.edu>
2016-03-06 23:53:33 -0500

    Updated command nodes

--- src/commandnode/BackwardNode.java
+++ src/commandnode/BackwardNode.java
@@ -7,14 +7,14 @@ public class BackwardNode extends UnaryNode {
 
 	public double evaluate(SLogoCharacterState currentState) throws SLogoException {
 		double[] newCoor = calculateLoc(currentState.getDirection(), currentState);
-		currentState.setXCoor(currentState.getXCoor() + newCoor[0]);
-		currentState.setYCoor(currentState.getYCoor() - newCoor[1]);
+		currentState.setXCoor(boundCoor(currentState.getXCoor() - newCoor[0]));
+		currentState.setYCoor(boundCoor(currentState.getYCoor() - newCoor[1]));
 		return getChildren().get(0).evaluate(currentState);
 	}
 
 	private double[] calculateLoc(double direction, SLogoCharacterState state) throws SLogoException {
 		double[] result = new double[2];
-		result[0] = -Math.sin(direction) * getChildren().get(0).evaluate(state);
+		result[0] = Math.sin(direction) * getChildren().get(0).evaluate(state);
 		result[1] = -Math.cos(direction) * getChildren().get(0).evaluate(state);
 		return result;
 	}
