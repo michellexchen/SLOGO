fc87c453715ec40fce7500e0bedb0157a985c34c
Adam Tache <at201@duke.edu>
2016-03-09 00:23:13 -0500

    Default variable value of 0

--- src/commandnode/VariableNode.java
+++ src/commandnode/VariableNode.java
@@ -17,29 +17,31 @@ public class VariableNode extends NullaryNode{
 	public void setVarName(String varName){
 		this.varName = varName;
 	}
-	
+
 	public String getVarName(){
 		return varName;
 	}
-	
+
 	public void setValue(double value){
 		myValue = value;
 	}
-	
+
 	public double evaluate(SLogoCharacterState state){
-		return getVarMap().get(varName);
+		if(getVarMap().containsKey(varName))
+			return getVarMap().get(varName);
+		return 0;
 	}
-	
+
 	public SLogoWorkspace getWorkspace(){
 		return myWorkspace;
 	}
-	
+
 	public void setWorkspace(SLogoWorkspace ws){
 		myWorkspace = ws;
 	}
-	
+
 	public HashMap<String, Double> getVarMap(){
 		return myWorkspace.getMyVarMap();
 	}
-	
+
 }
