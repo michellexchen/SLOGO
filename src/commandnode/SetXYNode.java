b13ec0f94b7b3aa5731ce18779af4445c0e22129
AaronNewman <agn9@duke.edu>
2016-03-10 00:22:58 -0500

    Fixed command nodes

--- src/commandnode/SetXYNode.java
+++ src/commandnode/SetXYNode.java
@@ -7,14 +7,14 @@ public class SetXYNode extends BinaryNode {
 
 	public double evaluate(SLogoCharacterState state) throws SLogoException {
 		double distance = calculateDistance(state.getXCoor(), state.getYCoor(), state);
-		state.setXCoor(boundCoor(transformXCoor(getChildren().get(0).evaluate(state))));
-		state.setYCoor(boundCoor(transformYCoor(getChildren().get(1).evaluate(state))));
+		state.setXCoor(getChildren().get(0).evaluate(state));
+		state.setYCoor(getChildren().get(1).evaluate(state));
 		return distance;
 	}
 
 	private double calculateDistance(double x, double y, SLogoCharacterState state) throws SLogoException {
-		return Math.sqrt(Math.pow(boundCoor(transformXCoor(getChildren().get(0).evaluate(state))) - x, 2)
-				- Math.pow(boundCoor(transformYCoor(getChildren().get(1).evaluate(state))) - y, 2));
+		return Math.sqrt(Math.pow(getChildren().get(0).evaluate(state) - x, 2)
+				- Math.pow(getChildren().get(1).evaluate(state) - y, 2));
 	}
 	
 }
--- src/commandnode/TowardsXYNode.java
+++ src/commandnode/TowardsXYNode.java
@@ -4,10 +4,16 @@ import exception.SLogoException;
 import model.SLogoCharacterState;
 
 public class TowardsXYNode extends TurnNode {
+	
+	private int NUM_CHILDREN = 2;
+	
+	public TowardsXYNode() {
+		setNumChildren(NUM_CHILDREN);
+	}
 
 	public double calculateDir(SLogoCharacterState state) throws SLogoException {
-		double diffX = boundCoor(transformXCoor(getChildren().get(0).evaluate(state))) - state.getXCoor();
-		double diffY = boundCoor(transformYCoor(getChildren().get(1).evaluate(state))) - state.getYCoor();
+		double diffX = getChildren().get(0).evaluate(state) - state.getXCoor();
+		double diffY = getChildren().get(1).evaluate(state) - state.getYCoor();
 		if (diffX < 0) {
 			if (diffY < 0) {
 				 return 270 + Math.toDegrees(Math.atan(diffX / diffY));
@@ -18,7 +24,7 @@ public class TowardsXYNode extends TurnNode {
 			if (diffY < 0) {
 				 return 90 - Math.toDegrees(Math.atan(diffX / diffY));
 			}
-			return 90 + Math.toDegrees(Math.atan(diffX / diffY));
+			return Math.toDegrees(Math.atan(diffX/diffY));
 		}
 	}
 	
