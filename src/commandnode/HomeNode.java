b79b6dca906980aaee486c4221080081de20018b
AaronNewman <agn9@duke.edu>
2016-03-10 02:33:14 -0500

    Revised home command

--- src/commandnode/HomeNode.java
+++ src/commandnode/HomeNode.java
@@ -3,9 +3,16 @@ package commandnode;
 import exception.SLogoException;
 import model.SLogoCharacterState;
 
-public class HomeNode extends SetXYNode{
+public class HomeNode extends NullaryNode{
 
 	public double evaluate(SLogoCharacterState state) throws SLogoException {
-		return super.evaluate(state);
+		double distance = calculateDistance(state.getXCoor(), state.getYCoor(), state);
+		state.setXCoor(0);
+		state.setYCoor(0);
+		return distance;
+	}
+
+	private double calculateDistance(double x, double y, SLogoCharacterState state) throws SLogoException {
+		return Math.sqrt(Math.pow(x, 2) - Math.pow(y, 2));
 	}
 }
