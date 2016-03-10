f0e1bc339f989a76881442086735e59b73c795b6
Adam Tache <at201@duke.edu>
2016-03-10 03:22:50 -0500

    Color Loader

--- src/model/ColorLoader.java
+++ src/model/ColorLoader.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.IOException;
+import java.util.HashMap;
 import exception.SLogoException;
 import javafx.scene.paint.Color;
 
@@ -7,10 +9,24 @@ public class ColorLoader extends FileLoader{
 
 	private final String colorDirectory = "resources/resources";
 	private final String colorExtension = "Colors.properties";
+	private HashMap<Integer, double[]> myRGBMap = new HashMap<Integer, double[]>();
 	
 	public Color getColor(int index) throws SLogoException{
-		// TODO Auto-generated method stub
-				return null;
+		double[] rgb = myRGBMap.get(index);
+		Color myColor = new Color(rgb[0], rgb[1], rgb[2], 1);
+		setDirectory(colorDirectory);
+		setExtension(colorExtension);
+		try {
+			super.load();
+		} catch (IOException e) {
+			throw new SLogoException("Encountered an error loading the Color file");
+		}
+		return myColor;
+	}
+	
+	public void addRGB(int r, int g, int b){
+		double[] rgb = {r, g, b};
+		myRGBMap.put(myRGBMap.keySet().size(), rgb);
 	}
 	
 }
