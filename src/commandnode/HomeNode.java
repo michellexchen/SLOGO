b26a90d6b7e9c2f26af6c7aeb2767eecfd8b5c80
AaronNewman <agn9@duke.edu>
2016-03-10 01:37:56 -0500

    Added home command

---
+++ src/commandnode/HomeNode.java
@@ -0,0 +1,11 @@
+package commandnode;
+
+import exception.SLogoException;
+import model.SLogoCharacterState;
+
+public class HomeNode extends SetXYNode{
+
+	public double evaluate(SLogoCharacterState state) throws SLogoException {
+		return super.evaluate(state);
+	}
+}
