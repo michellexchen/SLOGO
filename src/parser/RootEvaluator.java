bb31a32aa395028e3fcf31be293b36550a74c794
Hunter <narendly@gmail.com>
2016-03-11 22:13:16 -0500

    RootEvaluator added back-merge conflict fix

---
+++ src/parser/RootEvaluator.java
@@ -0,0 +1,34 @@
+package parser;
+
+import java.util.List;
+
+import commandnode.Node;
+import exception.SLogoException;
+import model.SLogoCharacter;
+import model.SLogoWorkspace;
+
+public class RootEvaluator {
+	
+	private SLogoWorkspace myWorkspace;
+	
+	public RootEvaluator(SLogoWorkspace myWorkspace){
+		this.myWorkspace = myWorkspace;
+	}
+	
+	public double evaluateRoots(List<Node> myRoots) throws SLogoException {
+		double evaluation = 0;
+		System.out.println(myWorkspace);
+		List<SLogoCharacter> myCharacters = myWorkspace.getActiveTurtlesList(); //iterate only through the list of active turtles
+		for (Node myRoot : myRoots) {
+			//for (SLogoCharacter character : myCharacters) {
+			for(SLogoCharacter character : myWorkspace.getCharacterList()) { //have not debugged so don't iterate through active turtles
+				evaluation = myRoot.evaluate(character.getState());
+				myWorkspace.getObservableDataList().get(myCharacters.indexOf(character))
+						.updateData(character.getState());
+			}
+			System.out.println("Evaluation: " + evaluation);
+		}
+		return evaluation;
+	}
+	
+}
